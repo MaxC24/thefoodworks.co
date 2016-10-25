03abaf1f107e9e05e1b8f1e9583549d19402b8f3
Nav31 <nav31@users.noreply.github.com>
2016-10-24 20:30:58 -0400

    before I add mgoDB

---
+++ server/models/user.go
@@ -0,0 +1,12 @@
+package models
+
+type (
+	// This is will be our user
+	User struct {
+		Name     string
+		Email    string
+		Password string
+		Location string
+		ID       string
+	}
+)
--- server/server.go
+++ server/server.go
@@ -1,14 +1,46 @@
 // to run this: inside of your go workspace use the command `go run server.go` assuming youre at the proper level in the directory structure
 package main
 
-import "net/http"
+import (
+	// Standard library packages
+	"encoding/json"
+	"fmt"
+	"net/http"
+	// Third party packages
+	"github.com/Nav31/Food.Now/Models"
+	"github.com/julienschmidt/httprouter"
+)
 
 func main() {
-	http.HandleFunc("/", frontPageFunc)
-	// I can also add more routes here such as http.HandleFunc("/users/johnEappleSeed/mostFrequent", anotherFunc)
-	http.ListenAndServe(":8080", nil)
-}
-
-func frontPageFunc(w http.ResponseWriter, req *http.Request) { // w http.ResponseWriter is going to write back to the request and req is the request
-	w.Write([]byte("Welcome to Food.Now"))
+	router := httprouter.New()
+	// Get req
+	router.GET("/user/:id", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		// Made a test User
+		user := models.User{
+			Name:     "Sergy Brin",
+			Email:    "sergy@google.com",
+			Password: "hello_world",
+			Location: "Menlo Park, CA",
+			ID:       p.ByName("id"),
+		}
+		uj, _ := json.Marshal(user)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "%s", uj)
+	})
+	// Post req
+	router.POST("/user", func(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
+		user := models.User{}
+		json.NewDecoder(req.Body).Decode(&user)
+		user.ID = "yello"
+		uj, _ := json.Marshal(user)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(201)
+		fmt.Fprintf(w, "%s", uj)
+	})
+	// Delete req
+	router.DELETE("/user/:id", func(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
+		w.WriteHeader(200)
+	})
+	http.ListenAndServe("localhost:3000", router)
 }
